3b508514ab1c0192f2b29b2d7c05636e8d244b6a
Rohan <rohan.sthill@gmail.com>
2023-05-07 16:59:18 +1000

    Initial commit

---
+++ src/gen/general.ml
@@ -0,0 +1,1 @@
+(*write a function that folds but keeps value sin a list*)
---
+++ src/interpextrap/polynomial.ml
@@ -0,0 +1,5 @@
+(* Nevilles algorithm method*)
+
+(* also do the NASA adapted one called iterative interpolation and numerical differentiation using divided differences*)
+let polint_neville x y xeval =
+               
